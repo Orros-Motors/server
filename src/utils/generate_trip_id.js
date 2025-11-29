038d7fad8ab9ec28daad229660c9dac7a01a9e17
ibeneme <ikennaibenemee@gmail.com>
2025-11-29 18:03:36 +0100

    updates

--- src/utils/generate_trip_id.js
+++ src/utils/generate_trip_id.js
@@ -1,12 +1,22 @@
-export const generateTripId = () => {
+// utils/generateTripId.js
+
+/**
+ * Generates a unique Trip ID
+ * Format: TRIP-YYYYMMDDHHMMSS-RAND
+ */
+const generateTripId = () => {
   const now = new Date();
 
+  // Format date as YYYYMMDDHHMMSS
   const datePart = now
     .toISOString()
     .replace(/[-:T.Z]/g, "")
     .slice(0, 14);
 
+  // Generate a random 4-character uppercase string
   const randomPart = Math.random().toString(36).substring(2, 6).toUpperCase();
 
   return `TRIP-${datePart}-${randomPart}`;
 };
+
+module.exports = { generateTripId };
